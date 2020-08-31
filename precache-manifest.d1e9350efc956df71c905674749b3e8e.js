11134d74ac9f4ec09eaf000926b37b391c47efb3
Gabriel Guarnieri Cardoso <gabreguarnieri@gmail.com>
2020-08-31 19:46:15 +0100

    Updates

---
+++ precache-manifest.d1e9350efc956df71c905674749b3e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5264cd20970cbef261d2662f0ea6ed9",
+    "url": "/music-trumps-front-end/index.html"
+  },
+  {
+    "revision": "535c7a82ba37b7d669ff",
+    "url": "/music-trumps-front-end/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "308f29e0329ae9be9db8",
+    "url": "/music-trumps-front-end/static/js/2.634aabf1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/music-trumps-front-end/static/js/2.634aabf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535c7a82ba37b7d669ff",
+    "url": "/music-trumps-front-end/static/js/main.15d2776b.chunk.js"
+  },
+  {
+    "revision": "08bc7d388514e9a759e7",
+    "url": "/music-trumps-front-end/static/js/runtime-main.69172d3d.js"
+  }
+]);
